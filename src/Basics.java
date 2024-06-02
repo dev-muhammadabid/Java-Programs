b53426d9431acd1ca2b29932fbb13a062b847ca5
dev-muhammadabid <muhammadabid.dev@gmail.com>
2024-06-02 08:21:35 +0530

    Add Programs

--- src/Basics.java
+++ src/Basics.java
@@ -191,3 +191,22 @@ Scanner sc = new Scanner(System.in);
     System.out.println(fact);
 
 */
+
+
+/*
+11. Fibonacci Program
+
+        int a = 0;
+        int b = 1;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Fibonacci n Num: ");
+        int fib = sc.nextInt();
+        System.out.print(a + b + " ");
+        for (int i = 2; i <= fib; i++){
+            int c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+
+*/
